efac5be87020a2af0d5bce81e07ff3c3c35fb3e0
Abha0981 <103839046+Abha0981@users.noreply.github.com>
2023-02-25 13:06:04 +0530

    Blog

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,11 @@ import "./footer.css"
 export default function Footer() {
   return (
     <div className="footer">
-        <span className="copyright">All rights are Reserved @ copyright 2023</span>
+      <span className="copyrightsreserved">All rights are Reserved </span>
+      <span>
+        <i className="copyrighticon fa-regular fa-copyright"></i>
+      </span>
+      <span className="year"> copyright {new Date().getFullYear()} </span>{/*2023*/}
     </div>
   )
 }
