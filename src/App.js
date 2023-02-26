9ef4766a5830c7ef17e6c7376b160b74fb462148
Abha0981 <103839046+Abha0981@users.noreply.github.com>
2023-02-26 23:20:43 +0530

    Blog

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 //import Home from "./pages/home/Home";
-import TopBar from "./components/topbar/TopBar";
-import Single from "./pages/single/Single";
+//import TopBar from "./components/topbar/TopBar";
+//import Single from "./pages/single/Single";
+
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-      <TopBar />
+      {/* <TopBar /> */}
       {/* <Home /> */}
-      <Single/>
+      {/* <Single/> */}
+      <Login/>
     </>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,8 +2,16 @@ import './login.css'
 
 export default function Login() {
   return (
-    <div>
-      
+    <div className='login'>
+      <h1 className='logintitle'>Login</h1>
+      <form className='loginform'>
+          <label>Email</label>
+          <input type="text" placeholder='Email'/>
+          <label>Password</label>
+          <input type="password" placeholder='Password'/>
+          <button className='loginbtn'>Login</button>
+      </form>
+      <button className='register'>Register</button>
     </div>
   )
 }
